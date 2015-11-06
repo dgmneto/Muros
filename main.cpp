cb5948e882574b8c480e88d009fc1835c5c0dd8c
dgmneto <dgmneto@hotmail.com>
2015-11-06 20:35:11 -0300

    Input

--- main.cpp
+++ main.cpp
@@ -17,12 +17,5 @@ int main()
             cin >> mapa[i][j];
         }
     }
-    for(i = 0; i < n; i++)
-    {
-        for(j = 0; j < n; j++)
-        {
-            cout << mapa[i][j];
-        }
-    }
     return 0;
 }
