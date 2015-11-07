930fef57579ca301885643a551a111a176ac09fd
dgmneto <dgmneto@hotmail.com>
2015-11-06 21:13:45 -0300

    Beggining

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Casa.h>
 
 using namespace std;
 
@@ -9,13 +10,25 @@ int main()
     char **mapa = new char*[n];
     for(i = 0; i < n; i++)
         mapa[i] = new char[m];
+    Casa *inicio, *fim;
     int j;
     for(i = 0; i < n; i++)
     {
         for(j = 0; j < n; j++)
         {
-            cin >> mapa[i][j];
+            char a;
+            cin >> a;
+            if(a == 'T')
+            {
+                fim = new Casa(i, j, -1, 0);
+            }
+            else if(a == 'P')
+            {
+                inicio = new Casa(i, j, 0, -1);
+            }
+            mapa[i][j] = a;
         }
     }
+
     return 0;
 }
